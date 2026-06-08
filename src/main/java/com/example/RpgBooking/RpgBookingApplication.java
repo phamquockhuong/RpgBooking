74c50da93ea8c7ef92cf7ddc3cd4158fc38a1bb9
phamquockhuong <phamquockhuong2000@gmail.com>
2026-06-08 21:19:34 +0700

    Fix issue

--- src/main/java/com/example/RpgBooking/RpgBookingApplication.java
+++ src/main/java/com/example/RpgBooking/RpgBookingApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RpgRpgBookingApplication {
+public class RpgBookingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RpgBookingApplication.class, args);
